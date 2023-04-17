73bec541383cd9c8b88824f850eee48c54bfad37
VALERIA <valerialomaka04@gmail.com>
2023-04-17 12:09:47 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listEl = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  listEl.appendChild(itemEl); 
+}
+
