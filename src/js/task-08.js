f5841d9e18d5b791d4aa99de9f869baeab4e0454
VALERIA <valerialomaka04@gmail.com>
2023-04-18 16:35:07 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+   
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    
+    if (email === '' || password === '') {
+        return alert("FILL IN ALL THE FIELDS!");
+    }
+
+    console.log(
+        {
+            email: email,
+            password:password,
+        }
+    )
+    event.currentTarget.reset();
+}
