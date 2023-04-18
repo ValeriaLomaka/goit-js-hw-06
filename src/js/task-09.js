0788607fe4a31025c9764c32d3596c4d4b131b53
VALERIA <valerialomaka04@gmail.com>
2023-04-18 16:35:03 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+function handleButtonChangeColor(event){
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
+btnEl.addEventListener("click", handleButtonChangeColor);
+
