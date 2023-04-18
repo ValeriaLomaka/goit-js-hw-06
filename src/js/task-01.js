ff241be2769ea84f3b5060ca97beb67120ab3d27
VALERIA <valerialomaka04@gmail.com>
2023-04-18 19:11:51 +0300

    delete files

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const itemEl = document.querySelectorAll('.item');
-console.log(`Number of categories:${itemEl.length}`);
-
-itemEl.forEach(el => {
-    console.log(`Category:${el.firstElementChild.textContent}`);
-    console.log(`Elements:${el.lastElementChild.querySelectorAll('li').length}`);
-})
